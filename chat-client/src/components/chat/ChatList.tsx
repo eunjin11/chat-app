5c7c07f44384c73a18d31bf0ee189094871cf752
eunjin11 <whdmswls9207@gmail.com>
2024-12-02 16:41:08 +0900

    feat(ui)/#10: input form 추가, 닉네임에 따라 메세지 정렬 변경

--- chat-client/src/components/chat/ChatList.tsx
+++ chat-client/src/components/chat/ChatList.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { Button } from "../ui/button";
 import { io, Socket } from "socket.io-client";
 import { useParams } from "react-router-dom";
+import { Input } from "../ui/input";
+import { Avatar, AvatarImage } from "../ui/avatar";
 
 interface MessageProps {
   text: string;
@@ -14,7 +16,7 @@ const ChatList = () => {
   const [socket, setSocket] = useState<Socket | null>(null);
   const [messages, setMessages] = useState<MessageProps[]>([]);
   const [messageInput, setMessageInput] = useState("");
-  const username = "테스트";
+  const username = "테스트2";
 
   useEffect(() => {
     const newSocket = io("http://localhost:3001", {
@@ -55,26 +57,49 @@ const ChatList = () => {
 
   return (
     <>
-      {messages.map((msg, idx) => (
-        <div key={idx}>
+      <div className="flex items-center justify-between mb-6">
+        <div className="flex items-center gap-3">
+          <Avatar>
+            <AvatarImage src={"./../image/profile.png"} />
+          </Avatar>
           <div>
-            <span>{msg.username}</span>
-            <span>{msg.text}</span>
-            <span>{new Date(msg.timestamp).toLocaleTimeString()}</span>
+            <div className="font-semibold">채팅방 제목</div>
+            <div className="text-sm text-gray-500 p-0">채팅방 설명</div>
           </div>
         </div>
-      ))}
-
-      <form onSubmit={sendMessage}>
-        <div>
-          <input
-            type="text"
-            value={messageInput}
-            onChange={(e) => setMessageInput(e.target.value)}
-            placeholder="메시지를 입력하세요..."
-          />
-          <Button>전송</Button>
-        </div>
+      </div>
+      <div className="space-y-4 mb-6">
+        {messages.map((msg, idx) => (
+          <div
+            key={idx}
+            className={`flex ${
+              msg.username === "테스트" ? "justify-end" : "justify-start"
+            }`}
+          >
+            <div
+              className={`${
+                msg.username === "테스트"
+                  ? "bg-black text-white"
+                  : " bg-gray-100 text-black"
+              }  rounded-lg py-2 px-3 max-w-[80%]`}
+            >
+              {msg.text}
+            </div>
+          </div>
+        ))}
+      </div>
+      <form
+        onSubmit={sendMessage}
+        className="flex w-full items-center space-x-2"
+      >
+        <Input
+          type="text"
+          value={messageInput}
+          onChange={(e) => setMessageInput(e.target.value)}
+          placeholder="메시지를 입력하세요..."
+          className="flex-1"
+        />
+        <Button>전송</Button>
       </form>
     </>
   );
